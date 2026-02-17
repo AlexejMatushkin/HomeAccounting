6aea1d41aa55c9891533a5fdbf9fcbe9a53cd70b
AlexejMatushkin <matushkin.leha2018@gmail.com>
2026-02-17 19:09:36 +0500

    Changed the project structure

--- app/src/main/java/com/practicum/homeaccounting/presentation/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/practicum/homeaccounting/presentation/screens/welcome/WelcomeScreen.kt
@@ -1,30 +1,39 @@
-package com.practicum.homeaccounting
+package com.practicum.homeaccounting.presentation.screens.welcome
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.practicum.homeaccounting.R
+import com.practicum.homeaccounting.ui.theme.HomeAccountingTheme
 
 @Composable
 fun WelcomeScreen(modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = stringResource(R.string.app_name),
-            fontSize = 24.sp
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(bottom = 8.dp)
         )
         Text(
             text = stringResource(R.string.app_name_ru),
-            fontSize = 16.sp
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Button(
             onClick = {}
@@ -33,5 +42,13 @@ fun WelcomeScreen(modifier: Modifier = Modifier) {
                 text = stringResource(R.string.log_in_app)
             )
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomeScreenPreview() {
+    HomeAccountingTheme {
+        WelcomeScreen()
     }
 }
